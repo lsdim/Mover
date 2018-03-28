2ee515ca9695a9f7f4e69d694c44c16284d1a318
lsdim <webmasi@mail.ru>
2018-03-28 16:59:47 +0300

    add_menu

--- Mover/MainF.Designer.cs
+++ Mover/MainF.Designer.cs
@@ -29,8 +29,11 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainF));
             this.nI1 = new System.Windows.Forms.NotifyIcon(this.components);
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.bRem1 = new System.Windows.Forms.Button();
+            this.bAdd1 = new System.Windows.Forms.Button();
             this.Sec = new System.Windows.Forms.NumericUpDown();
             this.p1 = new System.Windows.Forms.Panel();
             this.GV1 = new System.Windows.Forms.DataGridView();
@@ -39,13 +42,19 @@
             this.Place = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Mask = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Stan = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.bRem2 = new System.Windows.Forms.Button();
+            this.bAdd2 = new System.Windows.Forms.Button();
             this.p2 = new System.Windows.Forms.Panel();
             this.GV2 = new System.Windows.Forms.DataGridView();
             this.Ndir = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Path = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Browse = new System.Windows.Forms.DataGridViewButtonColumn();
-            this.bAdd2 = new System.Windows.Forms.Button();
-            this.bAdd1 = new System.Windows.Forms.Button();
+            this.cMS1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.налаштуванняToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.журналПодійToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.вихідToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
@@ -54,10 +63,13 @@
             ((System.ComponentModel.ISupportInitialize)(this.GV1)).BeginInit();
             this.p2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.GV2)).BeginInit();
+            this.cMS1.SuspendLayout();
             this.SuspendLayout();
             // 
             // nI1
             // 
+            this.nI1.ContextMenuStrip = this.cMS1;
+            this.nI1.Icon = ((System.Drawing.Icon)(resources.GetObject("nI1.Icon")));
             this.nI1.Text = "notifyIcon1";
             this.nI1.Visible = true;
             // 
@@ -70,20 +82,45 @@
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.bRem1);
             this.splitContainer1.Panel1.Controls.Add(this.bAdd1);
             this.splitContainer1.Panel1.Controls.Add(this.Sec);
             this.splitContainer1.Panel1.Controls.Add(this.p1);
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.bRem2);
             this.splitContainer1.Panel2.Controls.Add(this.bAdd2);
             this.splitContainer1.Panel2.Controls.Add(this.p2);
             this.splitContainer1.Size = new System.Drawing.Size(500, 410);
             this.splitContainer1.SplitterDistance = 221;
             this.splitContainer1.TabIndex = 0;
             // 
+            // bRem1
+            // 
+            this.bRem1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bRem1.Location = new System.Drawing.Point(53, 187);
+            this.bRem1.Name = "bRem1";
+            this.bRem1.Size = new System.Drawing.Size(45, 23);
+            this.bRem1.TabIndex = 3;
+            this.bRem1.Text = "-";
+            this.bRem1.UseVisualStyleBackColor = true;
+            this.bRem1.Click += new System.EventHandler(this.BRem_Click);
+            // 
+            // bAdd1
+            // 
+            this.bAdd1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bAdd1.Location = new System.Drawing.Point(3, 187);
+            this.bAdd1.Name = "bAdd1";
+            this.bAdd1.Size = new System.Drawing.Size(44, 23);
+            this.bAdd1.TabIndex = 2;
+            this.bAdd1.Text = "+";
+            this.bAdd1.UseVisualStyleBackColor = true;
+            this.bAdd1.Click += new System.EventHandler(this.BAdd_Click);
+            // 
             // Sec
             // 
+            this.Sec.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.Sec.Location = new System.Drawing.Point(150, 190);
             this.Sec.Name = "Sec";
             this.Sec.Size = new System.Drawing.Size(45, 20);
@@ -155,6 +192,28 @@
             "8 - видалити всі крім вказаних"});
             this.Stan.Name = "Stan";
             // 
+            // bRem2
+            // 
+            this.bRem2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bRem2.Location = new System.Drawing.Point(53, 142);
+            this.bRem2.Name = "bRem2";
+            this.bRem2.Size = new System.Drawing.Size(45, 23);
+            this.bRem2.TabIndex = 2;
+            this.bRem2.Text = "-";
+            this.bRem2.UseVisualStyleBackColor = true;
+            this.bRem2.Click += new System.EventHandler(this.BRem_Click);
+            // 
+            // bAdd2
+            // 
+            this.bAdd2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.bAdd2.Location = new System.Drawing.Point(3, 142);
+            this.bAdd2.Name = "bAdd2";
+            this.bAdd2.Size = new System.Drawing.Size(44, 23);
+            this.bAdd2.TabIndex = 1;
+            this.bAdd2.Text = "+";
+            this.bAdd2.UseVisualStyleBackColor = true;
+            this.bAdd2.Click += new System.EventHandler(this.BAdd_Click);
+            // 
             // p2
             // 
             this.p2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
@@ -203,25 +262,47 @@
             this.Browse.Text = "...";
             this.Browse.ToolTipText = "Обрати папку";
             // 
-            // bAdd2
+            // cMS1
             // 
-            this.bAdd2.Location = new System.Drawing.Point(3, 142);
-            this.bAdd2.Name = "bAdd2";
-            this.bAdd2.Size = new System.Drawing.Size(44, 23);
-            this.bAdd2.TabIndex = 1;
-            this.bAdd2.Text = "+";
-            this.bAdd2.UseVisualStyleBackColor = true;
-            this.bAdd2.Click += new System.EventHandler(this.bAdd_Click);
+            this.cMS1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.налаштуванняToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.журналПодійToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.вихідToolStripMenuItem});
+            this.cMS1.Name = "cMS1";
+            this.cMS1.ShowImageMargin = false;
+            this.cMS1.Size = new System.Drawing.Size(133, 104);
             // 
-            // bAdd1
+            // налаштуванняToolStripMenuItem
             // 
-            this.bAdd1.Location = new System.Drawing.Point(3, 187);
-            this.bAdd1.Name = "bAdd1";
-            this.bAdd1.Size = new System.Drawing.Size(44, 23);
-            this.bAdd1.TabIndex = 2;
-            this.bAdd1.Text = "+";
-            this.bAdd1.UseVisualStyleBackColor = true;
-            this.bAdd1.Click += new System.EventHandler(this.bAdd_Click);
+            this.налаштуванняToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.налаштуванняToolStripMenuItem.Name = "налаштуванняToolStripMenuItem";
+            this.налаштуванняToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.налаштуванняToolStripMenuItem.Text = "Налаштування";
+            // 
+            // журналПодійToolStripMenuItem
+            // 
+            this.журналПодійToolStripMenuItem.Name = "журналПодійToolStripMenuItem";
+            this.журналПодійToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.журналПодійToolStripMenuItem.Text = "Журнал подій";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(129, 6);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(129, 6);
+            // 
+            // вихідToolStripMenuItem
+            // 
+            this.вихідToolStripMenuItem.Name = "вихідToolStripMenuItem";
+            this.вихідToolStripMenuItem.Size = new System.Drawing.Size(132, 22);
+            this.вихідToolStripMenuItem.Text = "Вихід";
+            this.вихідToolStripMenuItem.Click += new System.EventHandler(this.вихідToolStripMenuItem_Click);
             // 
             // MainF
             // 
@@ -229,7 +310,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(500, 410);
             this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainF";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mover";
             this.Load += new System.EventHandler(this.MainF_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
@@ -240,6 +323,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.GV1)).EndInit();
             this.p2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.GV2)).EndInit();
+            this.cMS1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -263,6 +347,14 @@
         private System.Windows.Forms.DataGridViewButtonColumn Browse;
         private System.Windows.Forms.Button bAdd2;
         private System.Windows.Forms.Button bAdd1;
+        private System.Windows.Forms.Button bRem1;
+        private System.Windows.Forms.Button bRem2;
+        private System.Windows.Forms.ContextMenuStrip cMS1;
+        private System.Windows.Forms.ToolStripMenuItem налаштуванняToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem журналПодійToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem вихідToolStripMenuItem;
     }
 }
 
--- Mover/MainF.cs
+++ Mover/MainF.cs
@@ -18,18 +18,18 @@ namespace Mover
 
         private void MainF_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void GV2_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            if (e.ColumnIndex==2)
+            if (e.ColumnIndex == 2) //is column with buttons
             {
                 GV2_Btn_Click(e.RowIndex);
             }
         }
 
-        private void GV2_Btn_Click(int RowIndex)
+        private void GV2_Btn_Click(int RowIndex) //click button in Grid2
         {
             try
             {
@@ -40,7 +40,7 @@ namespace Mover
                 DialogResult dr = fbd.ShowDialog();
                 if (dr == DialogResult.OK)
                 {
-                    if (Gv2GetUnic(fbd.SelectedPath, RowIndex))
+                    if (Gv2IsUnic(fbd.SelectedPath, RowIndex))
                         GV2[1, RowIndex].Value = fbd.SelectedPath;
                 }
             }
@@ -51,7 +51,7 @@ namespace Mover
 
         }
 
-        private bool Gv2GetUnic(string text, int rowindex)
+        private bool Gv2IsUnic(string text, int rowindex) //Check is unic record in Grid2
         {
             bool rez = true;
 
@@ -59,7 +59,7 @@ namespace Mover
             {
                 if ((String)row.Cells[1].Value == text && row.Index != rowindex)
                 {
-                    MessageBox.Show(String.Format("Такий шлях вже вибрано в рядку № {0}",row.Cells[0].Value), "Увага", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    MessageBox.Show(String.Format("Такий шлях вже вибрано в рядку № {0}", row.Cells[0].Value), "Увага", MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     return false;
                 }
             }
@@ -67,7 +67,7 @@ namespace Mover
             return rez;
         }
 
-        private void bAdd_Click(object sender, EventArgs e)
+        private void BAdd_Click(object sender, EventArgs e) //buttons "+" Click
         {
             switch (((Button)sender).Name)
             {
@@ -78,7 +78,6 @@ namespace Mover
                     RowAdd(GV2);
                     break;
             }
-
         }
 
         private void RowAdd(DataGridView GV)
@@ -90,9 +89,42 @@ namespace Mover
 
         private void GV2_CellEndEdit(object sender, DataGridViewCellEventArgs e)
         {
-            Gv2GetUnic((String)GV2[e.ColumnIndex,e.RowIndex].Value, e.RowIndex);
+            Gv2IsUnic((String)GV2[e.ColumnIndex, e.RowIndex].Value, e.RowIndex);
         }
 
-        
+        private void BRem_Click(object sender, EventArgs e)
+        {
+            switch (((Button)sender).Name)
+            {
+                case "bRem1":
+                    RowRem(GV1);
+                    break;
+                case "bRem2":
+                    RowRem(GV2);
+                    break;
+            }
+        }
+
+        private void RowRem(DataGridView GV)
+        {
+            if (GV.Rows.Count <= 0)
+                return;
+
+            GV.Rows.Remove(GV.Rows[GV.CurrentCell.RowIndex]);
+
+            if (GV.Rows.Count <= 0)
+                return;
+
+            for (int i = GV.CurrentCell.RowIndex; i < GV.Rows.Count; i++)
+            {
+                GV[0, i].Value = i + 1;
+            }
+        }
+
+        private void вихідToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Ви дійсно бажаєте завершити роботу? ", "Mover", MessageBoxButtons.YesNo, MessageBoxIcon.Question) ==DialogResult.Yes)
+                Close();
+        }
     }
 }
